e6ea23f9bc4680885c0c261805222c61188283a1
jinow9595 <jinow9595@gmail.com>
2020-08-14 17:55:07 +0900

    Add UserController

--- UserController.java
+++ UserController.java
@@ -7,4 +7,12 @@ public class UserController {
 
 		return "user/login";
 	}
+
+	// 회원 추가 페이지로 이동
+	@RequestMapping("/user/insert.do")
+	public String insert() {
+		logger.info("경로 : insert");
+		
+		return "user/insert";
+	}
 }
